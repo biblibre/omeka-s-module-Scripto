c6ad59d2887b58257a9f914b0b5134ff09ad9dd0
Julian Maurice <julian.maurice@biblibre.com>
2026-03-19 09:07:24 +0100

    Add documentation
    This documentation is extracted from
    https://github.com/omeka/omeka-s-enduser
    
    It will receive modifications to reflect changes in the fork

---
+++ documentation/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Scripto'
+copyright = '2026, Omeka Team'
+author = 'Omeka Team'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', '.venv']
+
+language = 'en'
+
+myst_heading_anchors = 3
+myst_enable_extensions = [
+    "attrs_inline"
+]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_css_files = ['custom.css']
+
+gettext_compact = 'documentation'
---
+++ documentation/troubleshootingScripto.md
@@ -0,0 +1,20 @@
+# Troubleshooting
+
+The following are advanced options and troubleshooting tips for Scripto for Omeka S.
+
+## Disable anonymous editing
+By default, Scripto for Omeka S allows anonymous users to edit the content on your projects.
+
+To prevent this, you will need to edit the files in the directory containing your MediaWiki installation. Add the line
+
+```
+$wgGroupPermissions['*']['edit'] = false;
+```
+
+to the file `LocalSettings.php`.
+
+For more information, see the [MediaWiki manual on preventing access](https://www.mediawiki.org/wiki/Manual:Preventing_access){target=_blank}.
+
+## Troubleshooting
+### Project sync stuck
+If your project sync will not start or remains in process, check that you have [correctly configured the PHP-CLI path for your Omeka S installation](https://omeka.org/s/docs/user-manual/configuration/#php-path).
